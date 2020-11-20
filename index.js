6d5ff251d20379e37d56a2369962ebcfbec5c769
victory-a <victoryasokomeh@gmail.com>
2020-11-20 23:06:44 +0100

    improved accessibiliy, fixe tab scroll bug

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 // require('dotenv').config();
+// console.log(process.env.victory);
 
-console.log(process.env.victory);
+//selector functions
+const selectId = function (id) {
+    return document.getElementById(id);
+};
+
+const selectElement = function (element) {
+    return document.querySelector(element);
+};
+
+// elements
+let menuToggler = selectElement('.nav-toggle');
+console.log(menuToggler)
+let body = selectElement('body');
+
+// add eventlisteners
+menuToggler.addEventListener('click', function () {
+    body.classList.toggle('open')
+});
