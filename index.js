03715baded4625686b37c8dfc2ca02da311c8bd8
victory-a <victoryasokomeh@gmail.com>
2020-11-21 15:00:30 +0100

    completed tab section

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 // require('dotenv').config();
 // console.log(process.env.victory);
 
+const emptyStickyDiv = document.querySelector('.empty');
+console.log(emptyStickyDiv);
 function tobbleMobileNav() {
-  return document.getElementById("mobile-nav").classList.toggle('open');
+    return document.getElementById('mobile-nav').classList.toggle('open');
 }
 
-.empty 
+function scream() {
+    if (window.pageYOffset > 515) {
+        emptyStickyDiv.classList.add('visible');
+    } else {
+        emptyStickyDiv.classList.remove('visible');
+    }
+}
+
+window.addEventListener('scroll', () => scream());
