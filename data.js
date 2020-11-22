cf0d45552d4dce5a3d2284a7791f8f5a68c72cdd
victory-a <victoryasokomeh@gmail.com>
2020-11-22 19:47:28 +0100

    wrote initial js code for populating data and converted some vw dimensions

---
+++ data.js
@@ -0,0 +1,253 @@
+export const data = {
+  "data": {
+      "viewer": {
+          "avatarUrl": "https://avatars3.githubusercontent.com/u/28878343?s=300&u=ed33540e047c9190e7ac276fd11301fa3053d90e&v=4",
+          "name": "Victory Asokomeh",
+          "login": "victory-a",
+          "bio": "Frontend Developer & Mobile enthusiast\r\n",
+          "url": "https://github.com/victory-a",
+          "repositories": {
+              "totalCount": 24,
+              "nodes": [
+                  {
+                      "name": "trans_monitor",
+                      "description": null,
+                      "primaryLanguage": {
+                          "name": "JavaScript",
+                          "color": "#f1e05a"
+                      },
+                      "stargazerCount": 0,
+                      "forkCount": 0,
+                      "updatedAt": "2020-10-27T23:56:23Z",
+                      "url": "https://github.com/victory-a/trans_monitor"
+                  },
+                  {
+                      "name": "eslint-config-react-native-wcandillon",
+                      "description": "My eslint configuration for React Native ",
+                      "primaryLanguage": {
+                          "name": "JavaScript",
+                          "color": "#f1e05a"
+                      },
+                      "stargazerCount": 0,
+                      "forkCount": 0,
+                      "updatedAt": "2020-10-25T23:05:26Z",
+                      "url": "https://github.com/victory-a/eslint-config-react-native-wcandillon"
+                  },
+                  {
+                      "name": "react-native-firebase-social-app",
+                      "description": null,
+                      "primaryLanguage": {
+                          "name": "JavaScript",
+                          "color": "#f1e05a"
+                      },
+                      "stargazerCount": 0,
+                      "forkCount": 0,
+                      "updatedAt": "2020-10-25T22:51:43Z",
+                      "url": "https://github.com/victory-a/react-native-firebase-social-app"
+                  },
+                  {
+                      "name": "abeg-rebuild",
+                      "description": "A minimal rebuild of the abeg web interface.",
+                      "primaryLanguage": {
+                          "name": "JavaScript",
+                          "color": "#f1e05a"
+                      },
+                      "stargazerCount": 0,
+                      "forkCount": 0,
+                      "updatedAt": "2020-10-18T23:05:50Z",
+                      "url": "https://github.com/victory-a/abeg-rebuild"
+                  },
+                  {
+                      "name": "ego",
+                      "description": "Eyowo web clone (in progress)",
+                      "primaryLanguage": {
+                          "name": "JavaScript",
+                          "color": "#f1e05a"
+                      },
+                      "stargazerCount": 1,
+                      "forkCount": 0,
+                      "updatedAt": "2020-11-18T09:16:51Z",
+                      "url": "https://github.com/victory-a/ego"
+                  },
+                  {
+                      "name": "react-boilerplate",
+                      "description": "React setup with styled components and chakra ui, to be updated.....",
+                      "primaryLanguage": {
+                          "name": "JavaScript",
+                          "color": "#f1e05a"
+                      },
+                      "stargazerCount": 1,
+                      "forkCount": 0,
+                      "updatedAt": "2020-08-05T09:20:16Z",
+                      "url": "https://github.com/victory-a/react-boilerplate"
+                  },
+                  {
+                      "name": "buddy",
+                      "description": "Client app for social network project",
+                      "primaryLanguage": {
+                          "name": "JavaScript",
+                          "color": "#f1e05a"
+                      },
+                      "stargazerCount": 5,
+                      "forkCount": 1,
+                      "updatedAt": "2020-10-18T14:01:05Z",
+                      "url": "https://github.com/victory-a/buddy"
+                  },
+                  {
+                      "name": "manage",
+                      "description": "frontend mentor challenge ",
+                      "primaryLanguage": {
+                          "name": "HTML",
+                          "color": "#e34c26"
+                      },
+                      "stargazerCount": 0,
+                      "forkCount": 0,
+                      "updatedAt": "2020-06-28T08:14:34Z",
+                      "url": "https://github.com/victory-a/manage"
+                  },
+                  {
+                      "name": "insure",
+                      "description": "my submission for frontend mentor challenge",
+                      "primaryLanguage": {
+                          "name": "HTML",
+                          "color": "#e34c26"
+                      },
+                      "stargazerCount": 0,
+                      "forkCount": 0,
+                      "updatedAt": "2020-06-16T12:37:32Z",
+                      "url": "https://github.com/victory-a/insure"
+                  },
+                  {
+                      "name": "twitterClone",
+                      "description": null,
+                      "primaryLanguage": {
+                          "name": "TypeScript",
+                          "color": "#2b7489"
+                      },
+                      "stargazerCount": 0,
+                      "forkCount": 0,
+                      "updatedAt": "2020-08-26T22:25:37Z",
+                      "url": "https://github.com/victory-a/twitterClone"
+                  },
+                  {
+                      "name": "Josla-blog-deployed",
+                      "description": null,
+                      "primaryLanguage": {
+                          "name": "JavaScript",
+                          "color": "#f1e05a"
+                      },
+                      "stargazerCount": 0,
+                      "forkCount": 0,
+                      "updatedAt": "2020-02-21T08:50:56Z",
+                      "url": "https://github.com/victory-a/Josla-blog-deployed"
+                  },
+                  {
+                      "name": "twilio-1",
+                      "description": "practice using twilio api",
+                      "primaryLanguage": {
+                          "name": "JavaScript",
+                          "color": "#f1e05a"
+                      },
+                      "stargazerCount": 0,
+                      "forkCount": 0,
+                      "updatedAt": "2020-11-12T10:41:41Z",
+                      "url": "https://github.com/victory-a/twilio-1"
+                  },
+                  {
+                      "name": "Prettier-and-Eslint",
+                      "description": "My VS Code prettier and eslint configs",
+                      "primaryLanguage": null,
+                      "stargazerCount": 0,
+                      "forkCount": 0,
+                      "updatedAt": "2020-02-26T00:07:00Z",
+                      "url": "https://github.com/victory-a/Prettier-and-Eslint"
+                  },
+                  {
+                      "name": "Buddy-api",
+                      "description": "API for a social network app",
+                      "primaryLanguage": {
+                          "name": "JavaScript",
+                          "color": "#f1e05a"
+                      },
+                      "stargazerCount": 0,
+                      "forkCount": 0,
+                      "updatedAt": "2020-10-04T21:00:08Z",
+                      "url": "https://github.com/victory-a/Buddy-api"
+                  },
+                  {
+                      "name": "File-comparison-test",
+                      "description": null,
+                      "primaryLanguage": {
+                          "name": "JavaScript",
+                          "color": "#f1e05a"
+                      },
+                      "stargazerCount": 0,
+                      "forkCount": 0,
+                      "updatedAt": "2020-05-27T16:16:28Z",
+                      "url": "https://github.com/SomtoUgeh/File-comparison-test"
+                  },
+                  {
+                      "name": "ninja_api",
+                      "description": "api for a martial arts directory. Project intended for practicing stuff learnt from api masterclass",
+                      "primaryLanguage": {
+                          "name": "JavaScript",
+                          "color": "#f1e05a"
+                      },
+                      "stargazerCount": 0,
+                      "forkCount": 0,
+                      "updatedAt": "2020-02-04T06:14:54Z",
+                      "url": "https://github.com/victory-a/ninja_api"
+                  },
+                  {
+                      "name": "express-api-auth-starter",
+                      "description": null,
+                      "primaryLanguage": {
+                          "name": "JavaScript",
+                          "color": "#f1e05a"
+                      },
+                      "stargazerCount": 0,
+                      "forkCount": 0,
+                      "updatedAt": "2019-10-14T09:54:34Z",
+                      "url": "https://github.com/victory-a/express-api-auth-starter"
+                  },
+                  {
+                      "name": "REST-API-CARDIO",
+                      "description": null,
+                      "primaryLanguage": {
+                          "name": "JavaScript",
+                          "color": "#f1e05a"
+                      },
+                      "stargazerCount": 1,
+                      "forkCount": 0,
+                      "updatedAt": "2019-10-09T17:52:12Z",
+                      "url": "https://github.com/victory-a/REST-API-CARDIO"
+                  },
+                  {
+                      "name": "project-edward",
+                      "description": "A language translator app with speech recognition and text to speech. (React, Node, Express, MongoDB)",
+                      "primaryLanguage": {
+                          "name": "JavaScript",
+                          "color": "#f1e05a"
+                      },
+                      "stargazerCount": 3,
+                      "forkCount": 0,
+                      "updatedAt": "2020-02-03T15:54:17Z",
+                      "url": "https://github.com/victory-a/project-edward"
+                  },
+                  {
+                      "name": "smart-brain",
+                      "description": "Facial recognition app - Built with React, Node, Express  (Udemy 0 to hero course final project)",
+                      "primaryLanguage": {
+                          "name": "JavaScript",
+                          "color": "#f1e05a"
+                      },
+                      "stargazerCount": 1,
+                      "forkCount": 0,
+                      "updatedAt": "2020-01-25T23:48:05Z",
+                      "url": "https://github.com/victory-a/smart-brain"
+                  }
+              ]
+          }
+      }
+  }
+}
--- index.js
+++ index.js
@@ -1,13 +1,55 @@
-// require('dotenv').config();
-// console.log(process.env.victory);
+
+let data = {
+    "data": {
+        "viewer": {
+            "avatarUrl": "https://avatars3.githubusercontent.com/u/28878343?s=300&u=ed33540e047c9190e7ac276fd11301fa3053d90e&v=4",
+            "name": "Victory Asokomeh",
+            "login": "victory-a",
+            "bio": "Frontend Developer & Mobile enthusiast\r\n",
+            "url": "https://github.com/victory-a",
+            "repositories": {
+                "totalCount": 24,
+                "nodes": [
+                    {
+                        "name": "trans_monitor",
+                        "description": null,
+                        "primaryLanguage": {
+                            "name": "JavaScript",
+                            "color": "#f1e05a"
+                        },
+                        "stargazerCount": 0,
+                        "forkCount": 0,
+                        "updatedAt": "2020-10-27T23:56:23Z",
+                        "url": "https://github.com/victory-a/trans_monitor"
+                    },
+                    {
+                        "name": "eslint-config-react-native-wcandillon",
+                        "description": "My eslint configuration for React Native ",
+                        "primaryLanguage": {
+                            "name": "JavaScript",
+                            "color": "#f1e05a"
+                        },
+                        "stargazerCount": 0,
+                        "forkCount": 0,
+                        "updatedAt": "2020-10-25T23:05:26Z",
+                        "url": "https://github.com/victory-a/eslint-config-react-native-wcandillon"
+                    }
+                ]
+            }
+        }
+    }
+  }
+  
+  
+// console.log(data.data.viewer)
 
 // controls the functionality for toggling mobile nav
 function tobbleMobileNav() {
-  return document.getElementById('mobile-nav').classList.toggle('open');
+    return document.getElementById('mobile-nav').classList.toggle('open');
 }
 
 // controls the visibility tab bar avatar and username
-const emptyStickyDiv = document.querySelector('.empty');
+let emptyStickyDiv = document.querySelector('.empty');
 function toggleTabLeftSection() {
     if (window.pageYOffset > 515) {
         emptyStickyDiv.classList.add('visible');
@@ -16,4 +58,17 @@ function toggleTabLeftSection() {
     }
 }
 window.addEventListener('scroll', () => toggleTabLeftSection());
+
+function selectElementbyId(id) {
+    return document.getElementById(id);
+}
+// select elements to be populated
+let userAvatar = selectElementbyId('userAvatar');
+let userName = selectElementbyId('username');
+
+
+const { avatarUrl, bio, login, name, url, repositories } = data.data.viewer;
+
+
+
 
