b1e3393f4fd5746833fddd65078164f577c2dcb3
victory-a <victoryasokomeh@gmail.com>
2020-11-23 04:24:02 +0100

    logic for formatting repo list items

--- index.js
+++ index.js
@@ -1,47 +1,45 @@
-
 let data = {
-    "data": {
-        "viewer": {
-            "avatarUrl": "https://avatars3.githubusercontent.com/u/28878343?s=300&u=ed33540e047c9190e7ac276fd11301fa3053d90e&v=4",
-            "name": "Victory Asokomeh",
-            "login": "victory-a",
-            "bio": "Frontend Developer & Mobile enthusiast\r\n",
-            "url": "https://github.com/victory-a",
-            "repositories": {
-                "totalCount": 24,
-                "nodes": [
+    data: {
+        viewer: {
+            avatarUrl: 'https://avatars3.githubusercontent.com/u/28878343?s=300&u=ed33540e047c9190e7ac276fd11301fa3053d90e&v=4',
+            name: 'Victory Asokomeh',
+            login: 'victory-a',
+            bio: 'Frontend Developer & Mobile enthusiast\r\n',
+            url: 'https://github.com/victory-a',
+            repositories: {
+                totalCount: 24,
+                nodes: [
                     {
-                        "name": "trans_monitor",
-                        "description": null,
-                        "primaryLanguage": {
-                            "name": "JavaScript",
-                            "color": "#f1e05a"
+                        name: 'twitterClone',
+                        description: 'React setup with styled components and chakra ui, to be updated.....',
+
+                        primaryLanguage: {
+                            name: 'TypeScript',
+                            color: '#2b7489',
                         },
-                        "stargazerCount": 0,
-                        "forkCount": 0,
-                        "updatedAt": "2020-10-27T23:56:23Z",
-                        "url": "https://github.com/victory-a/trans_monitor"
+                        stargazerCount: 0,
+                        forkCount: 0,
+                        updatedAt: '2020-08-26T22:25:37Z',
+                        url: 'https://github.com/victory-a/twitterClone',
                     },
                     {
-                        "name": "eslint-config-react-native-wcandillon",
-                        "description": "My eslint configuration for React Native ",
-                        "primaryLanguage": {
-                            "name": "JavaScript",
-                            "color": "#f1e05a"
+                        name: 'twitterClone',
+                        description: 'React setup with styled components and chakra ui, to be updated.....',
+
+                        primaryLanguage: {
+                            name: 'TypeScript',
+                            color: '#2b7489',
                         },
-                        "stargazerCount": 0,
-                        "forkCount": 0,
-                        "updatedAt": "2020-10-25T23:05:26Z",
-                        "url": "https://github.com/victory-a/eslint-config-react-native-wcandillon"
-                    }
-                ]
-            }
-        }
-    }
-  }
-  
-  
-// console.log(data.data.viewer)
+                        stargazerCount: 0,
+                        forkCount: 0,
+                        updatedAt: '2020-08-26T22:25:37Z',
+                        url: 'https://github.com/victory-a/twitterClone',
+                    },
+                ],
+            },
+        },
+    },
+};
 
 // controls the functionality for toggling mobile nav
 function toggleMobileNav() {
@@ -57,18 +55,81 @@ function toggleTabLeftSection() {
         emptyStickyDiv.classList.remove('visible');
     }
 }
+
+// keeps track of vertical scroll position to determine elements to render on the categories tab
 window.addEventListener('scroll', () => toggleTabLeftSection());
 
-function selectElementbyId(id) {
-    return document.getElementById(id);
-}
 // select elements to be populated
-let userAvatar = selectElementbyId('userAvatar');
-let userName = selectElementbyId('username');
+let userAvatars = document.querySelectorAll('.userAvatar');
+let currentUser = document.querySelectorAll('#name');
+let userName = document.querySelectorAll('#username');
+let userBio = document.getElementById('bio');
+let repoCount = document.getElementById('private-repo-count');
+let reposContainer = document.getElementById('repos');
+
+function formatRepoLayout(repo) {
+    // template elements
+    const starButton = repo.viewerHasStarred
+        ? `<button class="star">
+            <span class="iconify" data-icon="octicon:star-fill-24" data-inline="false"></span>
+            Unstar
+            </button>`
+        : `<button class="star">
+            <span class="iconify" data-icon="octicon:star-24" data-inline="false"></span>
+                Star
+                </button>`;
+
+    const stars =
+        repo.stargazerCount > 0
+            ? `<span class="stargazers">
+            <a href="" aria-label="repo star stats">
+            <span class="iconify starred" data-icon="octicon:star-24" data-inline="false"></span>
+            </a>
+            <p class="stargazer-count">${repo.stargazerCount}</p>
+        </span>`
+            : '';
+
+    const forks =
+        repo.forkCount > 0
+            ? `<span class="forks">
+            <a href="" aria-label="repo forks">
+            <span class="iconify" data-icon="octicon:git-fork-24" data-inline="false"></span>
+            </a>
+            <p class="fork-count">${repo.forkCount}</p>
+            </span>`
+            : '';
+
+    const description = repo.description ? `<p class="repo-description">${repo.description}</p>` : '';
+
+    return `
+            <div class="repo-title-row">
+            <a class="repo-name" href=${repo.url}>${repo.name}</a>
+            ${starButton}
+            </div>
+            ${description}
+            <div class="repo-metadata">
+            <span class="repo-language-details">
+            <span class="language-color-code" style="background-color: ${repo.primaryLanguage.color};"></span>
+            <p class="language">${repo.primaryLanguage.name}</p>
+            </span>
+            ${stars}
+            ${forks}
+            </div>
+            `;
+}
+
+const { avatarUrl, login, name, bio, url, repositories } = data.data.viewer;
+
+// populates attributes element attributes and textcontents
+userAvatars.forEach((avatar) => (avatar.src = avatarUrl));
+currentUser.forEach((occurence) => (occurence.textContent = name));
+userName.forEach((occurence) => (occurence.textContent = login));
+repoCount.textContent = repositories.totalCount;
+userBio.textContent = bio;
 
 
-const { avatarUrl, bio, login, name, url, repositories } = data.data.viewer;
-
-
-
-
+repositories.nodes.forEach((repo) => {
+    const list = document.createElement('li');
+    list.innerHTML = formatRepoLayout(repo);
+    reposContainer.appendChild(list);
+});
